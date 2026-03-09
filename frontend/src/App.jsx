7f8f5da5cb8f894185c01084b9b84320f2af4037
adrianois <68710590+adrianois@users.noreply.github.com>
2026-03-09 18:27:17 -0400

    fix: remover acesso à rota /clinicas para usuário normal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import PrivateRoute from './components/PrivateRoute'
 
-import Login           from './pages/Login'
-import Register       from './pages/Register'
+import Login               from './pages/Login'
+import Register            from './pages/Register'
 import AguardandoAprovacao from './pages/AguardandoAprovacao'
-import NaoAutorizado  from './pages/NaoAutorizado'
+import NaoAutorizado       from './pages/NaoAutorizado'
 
-import Dashboard      from './pages/Dashboard'
-import Pacientes      from './pages/Pacientes'
-import Consultas      from './pages/Consultas'
-import Prontuarios    from './pages/Prontuarios'
+import Dashboard   from './pages/Dashboard'
+import Pacientes   from './pages/Pacientes'
+import Consultas   from './pages/Consultas'
+import Prontuarios from './pages/Prontuarios'
 
 import AdminDashboard from './pages/admin/AdminDashboard'
 import AdminClinicas  from './pages/admin/AdminClinicas'
@@ -25,26 +25,29 @@ export default function App() {
       <AuthProvider>
         <Routes>
           {/* Públicas */}
-          <Route path='/'                       element={<Login />} />
-          <Route path='/login'                  element={<Login />} />
-          <Route path='/register'               element={<Register />} />
-          <Route path='/aguardando-aprovacao'   element={<AguardandoAprovacao />} />
-          <Route path='/nao-autorizado'         element={<NaoAutorizado />} />
+          <Route path='/'                     element={<Login />} />
+          <Route path='/login'                element={<Login />} />
+          <Route path='/register'             element={<Register />} />
+          <Route path='/aguardando-aprovacao' element={<AguardandoAprovacao />} />
+          <Route path='/nao-autorizado'       element={<NaoAutorizado />} />
 
-          {/* Normal + Gestor */}
+          {/* Rotas de usuário normal */}
           <Route path='/dashboard'   element={<PrivateRoute perfis={['normal','gestor','admin']}><Dashboard /></PrivateRoute>} />
           <Route path='/pacientes'   element={<PrivateRoute perfis={['normal','gestor','admin']}><Pacientes /></PrivateRoute>} />
           <Route path='/consultas'   element={<PrivateRoute perfis={['normal','gestor','admin']}><Consultas /></PrivateRoute>} />
           <Route path='/prontuarios' element={<PrivateRoute perfis={['normal','gestor','admin']}><Prontuarios /></PrivateRoute>} />
 
+          {/* Clínicas: apenas admin e gestor */}
+          <Route path='/clinicas' element={<PrivateRoute perfis={['admin','gestor']}><AdminClinicas /></PrivateRoute>} />
+
           {/* Admin */}
-          <Route path='/admin'           element={<PrivateRoute perfis={['admin']}><AdminDashboard /></PrivateRoute>} />
-          <Route path='/admin/clinicas'  element={<PrivateRoute perfis={['admin']}><AdminClinicas /></PrivateRoute>} />
-          <Route path='/admin/usuarios'  element={<PrivateRoute perfis={['admin']}><AdminUsuarios /></PrivateRoute>} />
+          <Route path='/admin'          element={<PrivateRoute perfis={['admin']}><AdminDashboard /></PrivateRoute>} />
+          <Route path='/admin/clinicas' element={<PrivateRoute perfis={['admin']}><AdminClinicas /></PrivateRoute>} />
+          <Route path='/admin/usuarios' element={<PrivateRoute perfis={['admin']}><AdminUsuarios /></PrivateRoute>} />
 
           {/* Gestor */}
-          <Route path='/gestor'           element={<PrivateRoute perfis={['gestor','admin']}><GestorDashboard /></PrivateRoute>} />
-          <Route path='/gestor/usuarios'  element={<PrivateRoute perfis={['gestor','admin']}><GestorUsuarios /></PrivateRoute>} />
+          <Route path='/gestor'          element={<PrivateRoute perfis={['gestor','admin']}><GestorDashboard /></PrivateRoute>} />
+          <Route path='/gestor/usuarios' element={<PrivateRoute perfis={['gestor','admin']}><GestorUsuarios /></PrivateRoute>} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
